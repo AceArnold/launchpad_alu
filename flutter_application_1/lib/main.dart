b8657ace5e591fdcb7bb5a4013e5d482aa4538d0
Arnold <a.mutara@alustudent.com>
2026-07-10 17:55:51 +0200

    fifth code base

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'screens/auth/login_screen.dart';
 // ignore: unused_import
 import '../screens/student/student_shell.dart';
